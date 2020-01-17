b262feff1f146b00a4132101ea93d276e0988d7f
kijie <itay.kigel@gmail.com>
2020-01-17 21:26:40 +0200

    Basic Movement

---
+++ 2D Platformer/Assets/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public CharacterController2D controller;
+    private float horizontalMove = 0f;
+    public float runSpeed = 40f;
+    bool jump;
+    bool crouch;
+
+    private void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("crouch"))
+        {
+            crouch = true;
+        }else if (Input.GetButtonUp("crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
+
+

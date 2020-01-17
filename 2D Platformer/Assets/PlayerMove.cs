7761517dce4fd9a8bfbe48c8b314bc9a1f36d8d8
kijie <itay.kigel@gmail.com>
2020-01-17 23:07:59 +0200

    All Player animations

--- 2D Platformer/Assets/PlayerMove.cs
+++ 2D Platformer/Assets/PlayerMove.cs
@@ -9,21 +9,29 @@ public class PlayerMove : MonoBehaviour
     public float runSpeed = 40f;
     bool jump;
     bool crouch;
+    public Animator animator;
 
     private void Update()
     {
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
 
         if (Input.GetButtonDown("crouch"))
         {
             crouch = true;
-        }else if (Input.GetButtonUp("crouch"))
+            animator.SetBool("IsCrouch", true);
+        }
+        else if (Input.GetButtonUp("crouch"))
         {
             crouch = false;
+            animator.SetBool("IsCrouch", false);
         }
     }
 
@@ -31,6 +39,11 @@ public class PlayerMove : MonoBehaviour
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
         jump = false;
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("IsJumping", false);
     }
 }
 
